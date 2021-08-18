bb2bf08e86680f7f6dce5b56d84211989835237a
Matinahuel <matiasnahuel2396@gmail.com>
2021-08-17 22:35:35 -0300

    implemente multer en la carga de producto

--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -1,6 +1,11 @@
 var express = require('express');
+const path = require('path')
 var router = express.Router();
 const { productList, productDetail, cart, productAdd, productEdit, crear, actualizar, eliminar } = require('../controllers/productsController');
+
+/*     Multer     */
+
+const upload = require('../middlewares/productsMulter');
 
 
 /* GET Products  */
@@ -10,7 +15,7 @@ router.get('/productDetail/:id', productDetail);
 router.get('/cart', cart);
 
 router.get('/productAdd', productAdd);
-router.post('/productAdd', crear);
+router.post('/productAdd',upload.array('images'), crear);
 router.get('/productEdit/:id', productEdit);
 router.put('/productEdit/:id', actualizar);
 router.delete('/productDelete/:id', eliminar);
