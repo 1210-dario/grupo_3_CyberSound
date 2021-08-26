5196f6c8473264b2baa0134187b354110d4ec6e9
sebaraya <hugasebastian@gmail.com>
2021-08-26 19:10:24 -0300

    se cargaron cambios en header y vista login, y se aplico logica a la parte del check de recuerdame

--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -26,13 +26,13 @@ module.exports = {
             password: bcrypt.hashSync(password,10),
             condiciones: condiciones != undefined ? true : false,
             role: 'user',
-            image: 'default.png'
+            image: 'avatar.svg'
         }
         users.push(usuario)
 
         guardar(users);
         // A donde debe redirigir ? al home o a las config de usuario?
-        res.redirect('./register')
+        res.redirect('../')
     },
 
     findByEmail: (email)=>{
@@ -40,18 +40,24 @@ module.exports = {
     },
 
     userLogin: async  (req,res)=>{
-        console.log(users);
         const userLogin = users.find(user=>user.email === req.body.email && bcrypt.compareSync(req.body.password, user.password))
         if(!userLogin){
             //Aca se debe redireccionar al Login con el error('Credenciales incorrectas o usuario/password incorrectas')
             res.send('El usuario no existe o la contraseña es incorrecta')
         }
-        req.session.userLogin = userLogin;
-
+        req.session.userLogin = {
+            nombre : userLogin.nombre,
+            id : userLogin.id,
+            role : userLogin.role,
+            image : userLogin.image,
+            email : userLogin.email
+        }
+        if (req.body.remember != undefined){
         /* Aplicacion de Cookie */
-        res.cookie('usuario',userLogin.email, { maxAge: 60000 });
+        res.cookie('usuario',req.session.userLogin,{ maxAge: 60000 });    
+        }    
         // A donde debe redirigir ? al home o a las config de usuario?
         // Se manda la informacion osea la info del usuario logueado, como lo mostramos en la vista?   
-        res.redirect('./login', userLogin, title='login')
+        res.redirect('../')
     }
 }
--- site/src/middlewares/cookieAuthMiddleware.js
+++ site/src/middlewares/cookieAuthMiddleware.js
@@ -2,8 +2,15 @@ const { users } = require('../data/users/users');
 
 module.exports = (req,res,next) =>{
     if(req.cookies.usuario != undefined && req.session.userLogin == undefined){
-        const userLogin = users.find(user=>user.email === req.cookies.usuario)
-        req.session.userLogin = userLogin;
+        const userLogin = users.find(user=>user.email === req.cookies.usuario.email)
+        
+        req.session.userLogin = {
+            nombre : userLogin.nombre,
+            id : userLogin.id,
+            role : userLogin.role,
+            image : userLogin.image,
+            email : userLogin.email
+        }
     }
     next();
 }
