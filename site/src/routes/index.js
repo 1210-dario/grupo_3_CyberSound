2a72fa981cc797c0533fbb9c3d02f7d04760bdcb
Matinahuel <matiasnahuel2396@gmail.com>
2021-11-03 19:15:23 -0300

    cambios modelo banner

--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { index } = require('../controllers/indexController');
+const { index } = require('../controllers/IndexController');
 
 /* GET home page. */
 
